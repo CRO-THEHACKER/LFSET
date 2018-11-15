1af27a28036ad14c838f686b475f2c70c7919d09
Zachary <zacharyknox2026@gmail.com>
2018-11-14 19:35:27 -0800

    Fixed chmodf.py

--- chmodf.py
+++ chmodf.py
@@ -4,4 +4,13 @@ import time
 import os
 import sys
 
-os.chmod('')
+os.chmod('dl.py')
+os.chmod('chmodf.py')
+os.chmod('gflfs.py')
+os.chmod('gmd5hk.py')
+os.chmod('lfsinstallinst.py')
+os.chmod('mkdir.py')
+os.chmod('movdir.py')
+os.chmod('r.py')
+os.chmod('r.sh')
+os.system('python3 mkdir.py')
