37d2351dbbb216f5e803d7bbcf0362efda42b4d0
Zachary <zacharyknox2026@gmail.com>
2018-11-14 19:31:26 -0800

    Updated wgetftp.sh

--- wgetftp.sh
+++ wgetftp.sh
@@ -5,10 +5,10 @@ wget http://thedarkarmy.000webhostapp.com/Lfset/r.py
 wget http://thedarkarmy.000webhostapp.com/Lfset/r.sh
 wget http://thedarkarmy.000webhostapp.com/Lfset/dl.py
 wget http://thedarkarmy.000webhostapp.com/Lfset/gmd5hk.py
-wget http://thedarkarmy.000webhostapp.com/Lfetgflfs.py
-wget http://thedarkarmy.000webhostapp.com/lfsinstallinst.py
-wget http://thedarkarmy.000webhostapp.com/mkdir.py
-wget http://thedarkarmy.000webhostapp.com/movdir.py
+wget http://thedarkarmy.000webhostapp.com/Lfet/gflfs.py
+wget http://thedarkarmy.000webhostapp.com/Lfset/fsinstallinst.py
+wget http://thedarkarmy.000webhostapp.com/Lfset/mkdir.py
+wget http://thedarkarmy.000webhostapp.com/Lfset/movdir.py
 chmod +x wgetftp.sh
 chmod +x r.py
 chmod +X r.sh
