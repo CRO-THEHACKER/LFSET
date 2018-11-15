3654a7715956b17f9b0cb5d3a53a4f75c1c40d3c
Zachary <zacharyknox2026@gmail.com>
2018-11-15 12:57:42 -0800

    Update gflfs.py

--- gflfs.py
+++ gflfs.py
@@ -5,18 +5,7 @@ import system
 import os
 
 print("Getting 2 files, 'version-check.sh', 'lib-check.sh'...")
-time.sleep(5)
-os.system('clear')
-#version-check.sh
-file = open("version-check.sh","w")
-file.write("#!/bin/bash")
-file.write("export LC_ALL=C")
-file.write("bash --version | head -n1 | cut -d" " -f2-4")
-file.write("echo "/bin/sh -> `readlink -f /bin/sh`"")
-file.write("echo -n "Binutils: "; ld --version | head -n1 | cut -d" " 
--f3-")
-file.write("bison --version | head -n1")
-file.write("if [ -e /usr/bin/yacc ];")
-file.write("	then echo "/usr/bin/yacc -> `readlink -f 
-/usr/bin/yacc`")
-
+time.sleep(2)
+os.system('wget http://thedarkarmy.000webhostapp.com/Lfset/version-check.sh')
+os.system('wget http://thedarkarmy.000webhostapp.com/Lfset/lib-check.sh')
+os.system('python3 gmd5hk.py')
