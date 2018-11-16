63b228ca45228423121bc2e37d6a4d9253e07cfe
Zachary <zacharyknox2026@gmail.com>
2018-11-16 11:10:38 -0800

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 
 import os
-import system
+import sys
 import time
 
-os.system('chmod +x 
+print("Removeing: r.sh, r.py...")
+os.system('rm -rf r.sh && rm -rf s.py')
+print("Getting PKG's...")
+os.system('python3 gflfs.py')
+print("Getting MD5 Hash Key...")
+os.system('./gmd5hk.py')
+print("Running: lib-check.sh and then version-check.sh...")
+os.system('./wget.py')
+os.system('bash lib-check.sh')
+print("If files = 'not found' all good if files = 'All found' all good but, if all file = 'found this but not this install files. Look up on INET...")
+arefilesgood = raw_input('All good? (Y/n): ')
+if arefilesgood == 'n':
+	print("Look at the internet, please.")
+	os.system('exit')
+if arefilesgood == 'Y':
+	os.system('clear')
+	os.system('bash version-check.sh')
+	filesaregoodtwo = raw_input('Are all good? (N/y): ')
+	if filesaregoodtwo == 'y':
+		print('done for now...')
+	if filesaregoodtwo == 'N':
+		print("Get them' PKG's and run dl.py again.")
