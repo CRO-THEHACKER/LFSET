9847c979f0be374056b84bdf0f39d7a50e2b8458
Zachary <zacharyknox2026@gmail.com>
2018-11-16 05:51:22 -0800

    Made wget.py
    Get's lib-check.sh & version-check.sh for LFS..

---
+++ wget.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+os.system('wget http://thedarkarmy.000webhost.com/Lfset/version-check.sh')
+os.system('wget http://thedarkarmy.000webhost.com/Lfset/lib-check.sh')
